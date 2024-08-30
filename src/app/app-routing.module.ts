cf5cd5180e4a8a8c7e5cd0cba6a98c4b8c8078f3
SoniVega53 <svegam@miumg.edu.gt>
2024-08-30 15:50:27 -0600

    perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,16 @@ import { UserComponent } from './ui/user/user.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthenticatedGuard } from './auth/AuthenticatedGuard';
 import { RegisterComponent } from './ui/register/register.component';
+import { PerfilComponent } from './ui/perfil/perfil.component';
+import { ArticlesComponent } from './ui/articles/articles.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent, canActivate: [AuthGuard]},
   {path:"login",component:LoginComponent, canActivate: [AuthenticatedGuard]},
   {path:"register",component:RegisterComponent, canActivate: [AuthenticatedGuard]},
   {path:"users",component:UserComponent, canActivate: [AuthGuard]},
+  {path:"perfil",component:PerfilComponent, canActivate: [AuthGuard]},
+  {path:"articles",component:ArticlesComponent, canActivate: [AuthGuard]},
   { path: '**', redirectTo: '/home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { NavbarComponent } from './ui/navbar/navbar.component';
 import { LoginComponent } from './ui/login/login.component';
 import { UserComponent } from './ui/user/user.component';
 import { RegisterComponent } from './ui/register/register.component';
+import { ArticlesComponent } from './ui/articles/articles.component';
+import { PerfilComponent } from './ui/perfil/perfil.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { RegisterComponent } from './ui/register/register.component';
     LoginComponent,
     UserComponent,
     RegisterComponent,
+    ArticlesComponent,
+    PerfilComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [
